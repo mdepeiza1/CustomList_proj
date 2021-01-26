2acb2e20c053e70ef96bf2f899118216c9a18251
Key Admin <mdepeiza@keyconceptskb.com>
2021-01-26 10:29:31 -0500

    Added Capacity, Count, and the constructor to CustomList.

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -9,6 +9,26 @@ namespace CustomList
     public class CustomList<T>
     {
         // Step One: Member Variables, Properties, Constructor
+        private int count;
+        private T[] items;
+
+
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public int Capacity
+        {
+            get { return items.Length; } //may need to change this to a different method
+        }
+
+        public CustomList()
+        {
+            items = new T[4];
+        }
+           
         // Step Two: Indexer
         // Step Three: Add Method
     }
